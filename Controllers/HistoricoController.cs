86965a9b9043d2c8377be423280119a0f43547a5
Victor Augusto da Silva Lima <VLIMA06@jacto.local>
2023-10-05 21:08:00 -0300

    Area de Desempenho

--- Controllers/HistoricoController.cs
+++ Controllers/HistoricoController.cs
@@ -27,69 +27,114 @@ namespace ResolverQuestao.Controllers
         }
 
         [HttpGet("Index")]
-        public IActionResult Index()
+        public IActionResult Index(string filtro)
         {
 
-        var usuarioLogado = User.Identity.Name;
+            var usuarioLogado = User.Identity.Name;
+
+            var listaRegistros = _context.ListaRegistros.Where(x => x.UsuarioId == usuarioLogado).Include(x => x.ListaExercicio).ThenInclude(x => x.Exercicios).ThenInclude(x => x.Alternativas).ToList();
+
+            var listaExerciciosRespondidos = new List<ListaExercicio>();
+
+            var tiposListas = new List<string>();
+
+            foreach (var item in listaRegistros)
+            {
+                tiposListas.Add(item.ListaExercicio.Tipo);
+            }
+
+            //remover os tipos de lista repetidos
+
+            var tiposListasDistinct = tiposListas.Distinct().ToList();
+
+            //agora vamos criar um HistoricoMateria para cada tipo de lista
+
+            var listaHistoricoMateria = new List<HistoricoMateria>();
+
+            foreach (var item in tiposListasDistinct)
+            {
+                var historicoMateria = new HistoricoMateria
+                {
+                    NomeMateria = item
+                };
+
+                var listaRegistrosMateria = listaRegistros.Where(x => x.ListaExercicio.Tipo == item).ToList();
+
+                var quantidadeQuestoesCorretasMateria = 0;
+
+                foreach (var registro in listaRegistrosMateria)
+                {
+                    quantidadeQuestoesCorretasMateria += registro.Acertos;
+                }
+
+                var quantidadeQuestoesErradasMateria = 0;
+
+                foreach (var registro in listaRegistrosMateria)
+                {
+                    quantidadeQuestoesErradasMateria += registro.Erros;
+                }
+
+                var quantidadeQuestoesRespondidasMateria = quantidadeQuestoesCorretasMateria + quantidadeQuestoesErradasMateria;
+
+                var aproveitamentoMateria = quantidadeQuestoesCorretasMateria * 100 / quantidadeQuestoesRespondidasMateria;
+
+                historicoMateria.QuantidadeQuestoesCorretas = quantidadeQuestoesCorretasMateria;
+
+                historicoMateria.QuantidadeQuestoesErradas = quantidadeQuestoesErradasMateria;
+
+                historicoMateria.QuantidadeQuestoesRespondidas = quantidadeQuestoesRespondidasMateria;
+
+                historicoMateria.PorcentagemAcerto = aproveitamentoMateria.ToString() + "%";
+
+                //pegar a ultima data de resposta
+
+                var listaDatas = new List<DateTime>();
+
+                foreach (var registro in listaRegistrosMateria)
+                {
+                    listaDatas.Add(registro.DataRegistro);
+                }
+
+                var ultimaData = listaDatas.Max();
+
+                historicoMateria.UltimaDataResposta = ultimaData;
+
+
+
+                listaHistoricoMateria.Add(historicoMateria);
+            }
+            //inverter a lista para que a materia mais recente apareça primeiro
+
+            switch (filtro)
+            {
+                case "data":
+                    listaHistoricoMateria.Reverse();
+                    ViewBag.Filtro = "data invertida";
+                    break;
+
+                case "porcentagemAcerto":
+
+                    listaHistoricoMateria = listaHistoricoMateria.OrderByDescending(x => x.PorcentagemAcerto).ToList();
+                    ViewBag.filtroAcerto = "porcentagemAcerto";
+                    break;
+
+                case "porcentagemAcertoInvertida":
+
+                    listaHistoricoMateria = listaHistoricoMateria.OrderBy(x => x.PorcentagemAcerto).ToList();
+                    ViewBag.filtroAcerto = "porcentagemAcertoInvertida";
+                    break;
+                    
+                default:
+                    // code to handle all other cases
+                    break;
+            }
+
         
-        var listaRegistros = _context.ListaRegistros.Where(x => x.UsuarioId == usuarioLogado).Include(x => x.ListaExercicio).ThenInclude(x => x.Exercicios).ThenInclude(x => x.Alternativas).ToList();
-      
-        var listaExerciciosRespondidos = new List<ListaExercicio>();
-
-        var tiposListas = new List<string>();
-
-        foreach (var item in listaRegistros)
-        {
-            tiposListas.Add(item.ListaExercicio.Tipo);
-        }
-
-        //remover os tipos de lista repetidos
-
-        var tiposListasDistinct = tiposListas.Distinct().ToList();
-
-        //agora vamos criar um HistoricoMateria para cada tipo de lista
-
-        var listaHistoricoMateria = new List<HistoricoMateria>();
-
-        foreach (var item in tiposListasDistinct)
-        {
-            var historicoMateria = new HistoricoMateria
-            {
-                NomeMateria = item
-            };
-
-            var listaRegistrosMateria = listaRegistros.Where(x => x.ListaExercicio.Tipo == item).ToList();
-
-            var quantidadeQuestoesCorretasMateria = 0;
-
-            foreach (var registro in listaRegistrosMateria)
-            {
-                quantidadeQuestoesCorretasMateria += registro.Acertos;
-            }
-
-            var quantidadeQuestoesErradasMateria = 0;
-
-            foreach (var registro in listaRegistrosMateria)
-            {
-                quantidadeQuestoesErradasMateria += registro.Erros;
-            }
-
-            var quantidadeQuestoesRespondidasMateria = quantidadeQuestoesCorretasMateria + quantidadeQuestoesErradasMateria;
-
-            var aproveitamentoMateria = quantidadeQuestoesCorretasMateria * 100 / quantidadeQuestoesRespondidasMateria;
-
-            historicoMateria.QuantidadeQuestoesCorretas = quantidadeQuestoesCorretasMateria;
-
-            historicoMateria.QuantidadeQuestoesErradas = quantidadeQuestoesErradasMateria;
-
-            historicoMateria.QuantidadeQuestoesRespondidas = quantidadeQuestoesRespondidasMateria;
-
-            historicoMateria.PorcentagemAcerto = aproveitamentoMateria.ToString() + "%";
-
-            listaHistoricoMateria.Add(historicoMateria);
-        }
             return View(listaHistoricoMateria);
         }
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Models/HistoricoMateria.cs
+++ Models/HistoricoMateria.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,6 +16,10 @@ namespace ResolverQuestao.Models
         public int QuantidadeQuestoesRespondidas { get; set; }
 
         public string PorcentagemAcerto { get; set; }
+        
+
+        [NotMapped]
+        public DateTime UltimaDataResposta { get; set; }
 
     }
 }
